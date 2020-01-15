ad339c0c37d5b73ae8999f39221f5a191cc549ae
Vadzim Ulitski <Vadzim_Ulitski@epam.com>
2020-01-15 10:45:24 +0300

    [Feature] Adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from "./contact/contact.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { HelloWorldComponent } from "./hello-world/hello-world.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main',
+    component: HelloWorldComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '404',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainViewComponent,
+    NotFoundComponent,
+    ContactComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
